4a6adeba2103f50942919d37dde24ac37ba2080f
Murilo-28 <murilocasa28@gmail.com>
2026-06-29 21:48:29 -0300

    Loja completa quase pronta

--- MVC_GAMEHUB/Areas/ADM/Controllers/JogosController.cs
+++ MVC_GAMEHUB/Areas/ADM/Controllers/JogosController.cs
@@ -57,5 +57,103 @@ namespace MVC_GAMEHUB.Areas.ADM.Controllers
 
             return RedirectToAction("Loja", "Jogos", new { area = "ADM" });
         }
+
+        public async Task<IActionResult> Editar(int id)
+        {
+            var jogo = await _context.Jogos.FindAsync(id);
+            if (jogo == null) return NotFound();
+            return View(jogo);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Editar(int id, Jogo jogo)
+        {
+            if (!ModelState.IsValid) return View(jogo);
+
+            var jogoBanco = await _context.Jogos.FindAsync(id);
+            if (jogoBanco == null) return NotFound();
+
+            jogoBanco.Nome = jogo.Nome;
+            jogoBanco.Categoria = jogo.Categoria;
+            jogoBanco.Preco = jogo.Preco;
+            jogoBanco.ImagemUrl = jogo.ImagemUrl;
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Loja", "Jogos", new { area = "ADM", id = id });
+        }
+
+        public async Task<IActionResult> Detalhes(int id)
+        {
+            var jogo = await _context.Jogos.FindAsync(id);
+            if (jogo == null) return NotFound();
+            return View(jogo);
+        }
+
+        public IActionResult AdicionarCarrinho(int id)
+        {
+            var jogo = _context.Jogos.Find(id);
+            if (jogo == null) return NotFound();
+
+            var carrinho = HttpContext.Session.GetString("Carrinho");
+            var itens = carrinho != null
+                ? System.Text.Json.JsonSerializer.Deserialize<List<CarrinhoItem>>(carrinho)
+                : new List<CarrinhoItem>();
+
+            if (!itens.Any(i => i.JogoId == id))
+            {
+                itens.Add(new CarrinhoItem
+                {
+                    JogoId = jogo.Id,
+                    Nome = jogo.Nome,
+                    ImagemUrl = jogo.ImagemUrl,
+                    Preco = jogo.Preco
+                });
+            }
+
+            HttpContext.Session.SetString("Carrinho",
+                System.Text.Json.JsonSerializer.Serialize(itens));
+
+            return RedirectToAction("Carrinho");
+        }
+
+        public IActionResult Carrinho()
+        {
+            var carrinho = HttpContext.Session.GetString("Carrinho");
+            var itens = carrinho != null
+                ? System.Text.Json.JsonSerializer.Deserialize<List<CarrinhoItem>>(carrinho)
+                : new List<CarrinhoItem>();
+
+            return View(itens);
+        }
+
+        public IActionResult RemoverCarrinho(int id)
+        {
+            var carrinho = HttpContext.Session.GetString("Carrinho");
+            var itens = carrinho != null
+                ? System.Text.Json.JsonSerializer.Deserialize<List<CarrinhoItem>>(carrinho)
+                : new List<CarrinhoItem>();
+
+            itens.RemoveAll(i => i.JogoId == id);
+
+            HttpContext.Session.SetString("Carrinho",
+                System.Text.Json.JsonSerializer.Serialize(itens));
+
+            return RedirectToAction("Carrinho");
+        }
+
+        public IActionResult FinalizarCompra()
+        {
+            var carrinho = HttpContext.Session.GetString("Carrinho");
+            var itens = carrinho != null
+                ? System.Text.Json.JsonSerializer.Deserialize<List<CarrinhoItem>>(carrinho)
+                : new List<CarrinhoItem>();
+
+            if (!itens.Any()) return RedirectToAction("Carrinho");
+
+            HttpContext.Session.Remove("Carrinho");
+
+            return View();
+        }
     }
 }
---
+++ MVC_GAMEHUB/Models/Carrinho.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC_GAMEHUB.Models
+{
+    public class CarrinhoItem
+    {
+        public int JogoId { get; set; }
+        public string Nome { get; set; }
+        public string ImagemUrl { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
